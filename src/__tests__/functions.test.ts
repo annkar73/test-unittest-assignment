01f8c05531f544a78a4a26c06777a9d713991438
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2024-05-18 08:23:50 +0200

    addTodo test

--- src/__tests__/functions.test.ts
+++ src/__tests__/functions.test.ts
@@ -1,5 +1,6 @@
-import { removeAllTodos, changeTodo } from "../ts/functions";
+import { removeAllTodos, changeTodo, addTodo } from "../ts/functions";
 import { Todo } from "../ts/models/todo";
+import { IAddResponse } from "../ts/models/IAddResponse";
 
 describe("functions functions", () => {
     test("should remove all the todos from array", () => {
@@ -30,6 +31,26 @@ describe("functions functions", () => {
 
         changeTodo(todo);
         expect(todo.done).toBe(false);
+    });
+    test("should add a todo when text length is greater than 2", () => {
+        const todos: Todo[] = [];
+        const todoText = "Test Todo";
+
+        const result: IAddResponse = addTodo(todoText, todos);
+
+        expect(result).toEqual({ success: true, error: "" });
+        expect(todos.length).toBe(1);
+        expect(todos[0].text).toBe(todoText);
+        expect(todos[0].done).toBe(false);
+    });
+    test("should not add a todo when text length is 2 or less", () => {
+        const todos: Todo[] = [];
+        const todoText = "No";
+
+        const result: IAddResponse = addTodo(todoText, todos);
+
+        expect(result).toEqual({ success: false, error: "Du måste ange minst tre bokstäver" });
+        expect(todos.length).toBe(0);
     });
 
 });
